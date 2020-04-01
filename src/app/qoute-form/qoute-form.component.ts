90e4512b4d61feb1611123cbe9d661a4ba0064fb
margaret mwangi <margaret13mwangi@gmail.com>
2020-04-01 11:50:23 +0300

    modified files

--- src/app/qoute-form/qoute-form.component.ts
+++ src/app/qoute-form/qoute-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'//transmitting to quote component(parent)
 
 @Component({
-  selector: 'app-qoute-form',
-  templateUrl: './qoute-form.component.html',
-  styleUrls: ['./qoute-form.component.css']
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
 })
-export class QouteFormComponent implements OnInit {
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0, "", "", "", new Date());//format as defined by class property
+
+  @Output() addQuote = new EventEmitter<Quote>();//event emitter to listen and send out new quotes on quote parent component
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);//this triggers addition of the new quote
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
